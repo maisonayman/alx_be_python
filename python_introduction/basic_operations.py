6ff79e43e5054c7d3d58d928c185b17b5344ae2e
maisonayman <maisonsayed2002@gmail.com>
2025-07-13 23:43:08 +0300

    basic python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 5
+number2 = 5
+
+sum = number1 + number2
+
+difference = number1 - number2
+
+product = number1 * number2
+
+print("Addition of", number1 , "and" , number2 ,"is", sum ,
+       "\nSubtraction of" , number1 ,"and" , number2 , "is", difference ,
+        "\nMultiplication of", number1 , "and" , number2 , "is" , product )
