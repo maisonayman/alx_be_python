c3a27b8093367b315314f3c75114fd6529982a9d
maisonayman <maisonsayed2002@gmail.com>
2025-07-27 03:05:36 +0300

    new task

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation ( num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2 
+
+    elif operation == 'subtract':
+        result = num1 - num2
+
+    elif operation == 'multiply':
+        result = num1 * num2
+
+    elif operation == 'divide':
+        if num2 == 0:
+            result = "can't divide on 0"
+        else :
+            result = num1/num2
+
+    else :
+        result = "the operation is unknown"
+
+    return result
