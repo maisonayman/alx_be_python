924773c3f90606be8713a15e6aa4375b1bab4bfb
maisonayman <maisonsayed2002@gmail.com>
2025-08-10 22:41:01 +0300

    new task

---
+++ oop/library_system.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_info(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def display_info(self):
+        return f"EBook: {self.title} by {self.author} [E-Book, {self.file_size}MB]"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def display_info(self):
+        return f"PrintBook: {self.title} by {self.author} [Print Book, {self.page_count} pages]"
+
+
+class Library:
+    def __init__(self):
+        self.books = []  
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book.display_info())
